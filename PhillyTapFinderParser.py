9741ee696728e9fd11acb7aba3093c7cbc0a651c
Colin Haley <admin@colinhaley.net>
2016-06-20 18:25:27 -0400

    Adding some structure to this mess

--- PhillyTapFinderParser.py
+++ PhillyTapFinderParser.py
@@ -3,17 +3,33 @@
 import sys
 import os
 import datetime
-import BeautifulSoup
+from BeautifulSoup import BeautifulSoup,SoupStrainer
 import urllib2
 import httplib2
 
 web_page = "http://www.phillytapfinder.com/events/"
 
-soup = BeautifulSoup.Soup
-strainer = BeautifulSoup.Strainer
-
 http = httplib2.Http()
 
 status, response = http.request(web_page)
 
-variables = BeautifulSoup(response, parseOnlyThese=strainer('ul'))
+unordered_lists = BeautifulSoup(response, parseOnlyThese=SoupStrainer('ul'))
+
+for items in unordered_lists:
+    if 'href=/event/'
+
+
+
+
+
+
+class Event:
+
+    title = ""
+    location = ""
+    hyperlink = ""
+    date = datetime.date.today()
+
+    def __init__(self):
+        # something
+
