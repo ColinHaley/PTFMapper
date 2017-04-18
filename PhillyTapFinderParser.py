d97c4e6c56116611a2538c47e9a7ae76981ea94a
Colin Haley <admin@colinhaley.net>
2017-04-18 19:19:23 -0400

    Update PTF Mapper, lets try this again

--- PhillyTapFinderParser.py
+++ PhillyTapFinderParser.py
@@ -7,21 +7,6 @@ from BeautifulSoup import BeautifulSoup,SoupStrainer
 import urllib2
 import httplib2
 
-web_page = "http://www.phillytapfinder.com/events/"
-
-http = httplib2.Http()
-
-status, response = http.request(web_page)
-
-unordered_lists = BeautifulSoup(response, parseOnlyThese=SoupStrainer('ul'))
-
-for items in unordered_lists:
-    if 'href=/event/'
-
-
-
-
-
 
 class Event:
 
@@ -30,6 +15,30 @@ class Event:
     hyperlink = ""
     date = datetime.date.today()
 
-    def __init__(self):
-        # something
+    def __init__(self, Title, Location, HyperLink, Date):
+        title = Title
+        location = Location
+        hyperlink = HyperLink
+        date = Date
 
+def create_event():
+    return 0
+
+
+web_page = "http://www.phillytapfinder.com/events/"
+
+http = httplib2.Http()
+
+status, response = http.request(web_page)
+
+# This is the grid.
+payload = BeautifulSoup(response, parseOnlyThese=SoupStrainer('div',{'class':'results-grid tall-results'}))
+for line in payload.findAll('a'):
+    a = line.get('href')
+    b = line.get('title')
+    print (a + "" + b)
+    -#span stuff should go under findAll('a')
+spans = payload.findAll('span')
+spans[0].findAll('span')[0].getText('>')
+for x in spans[0].findAll('span'):
+    x.getText('>')
